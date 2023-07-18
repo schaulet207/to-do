f782ac45c890142cadf426f2de1c1eb957e938c3
Shannon Chaulet <shannon.chaulet@gmail.com>
2023-07-17 23:13:59 -0400

    Add more to basic page UI. Add more dom manipulation logic

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import { createToDo } from "./createList.js";
-import { domManip } from "./dom-manipulation.js";
+import { displayDefaultProject, displayTheForm, addItemtoChecklist, clearForm } from "./dom-manipulation.js";
 import { defaultProjects } from "./default-projects.js";
+import './style.css';
 
     // Call default projects on load
-    defaultProjects();
+    // defaultProjects();
 
     // Call DOM manipulation module to form UI
-    domManip();
+    defaultProjects();
+
+    // Click events module
+    let clickEventsModule = (function() {
+        // Click event for displaying the form
+        const addNewToDo = document.querySelector(".add-todo-button");
+        addNewToDo.addEventListener("click", displayTheForm);
+
+        // Click event for adding an item to the checklist on the form
+        const addToChecklist = document.querySelector(".add-to-checklist");
+        addToChecklist.addEventListener("click", addItemtoChecklist);
+
+        // Click event to clear the form
+        const clearButton = document.querySelector(".reset-button");
+        clearButton.addEventListener("click", clearForm);
+
+    })();
 
     // Create toDo list
     const myToDo = createToDo("Get Essentials", "Go buy water", "7/16/2023", "High", "Water, Food, Clothes");
